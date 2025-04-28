ea0142c57194a720c2a42cc07517a52c2b664aee
Mudit Gupta <muditgupta@Mudits-MacBook-Air.local>
2025-04-29 04:56:58 +0530

    fix

--- src/pages/ItemDetail.js
+++ src/pages/ItemDetail.js
@@ -1,10 +1,12 @@
 import Blits from "@lightningjs/blits";
 import { fetchItemDetail, fetchSimilarContent, getImageUrl } from "../api/api";
 import Background from "../components/Background";
+import Navbar from "../components/Navbar";
 
 export default Blits.Component("ItemDetail", {
   components: {
     Background,
+    Navbar
   },
   state() {
     return {
@@ -16,11 +18,13 @@ export default Blits.Component("ItemDetail", {
         overview: "",
         status: "",
         backdrop_path: "",
-        vote_average:0,
-        genres: [{
-          id: "",
-          name: ""
-        }],
+        vote_average: 0,
+        genres: [
+          {
+            id: "",
+            name: "",
+          },
+        ],
       },
       similarItems: [],
       itemId: "",
@@ -30,38 +34,41 @@ export default Blits.Component("ItemDetail", {
   },
   computed: {
     getReleaseDate() {
-      if(this.itemDetail.status === "Released") {
+      if (this.itemDetail.status === "Released") {
         return new Date(this.itemDetail.release_date).toLocaleString("en-US", {
           month: "long",
           year: "numeric",
-        })
+        });
       }
-      return "Coming Soon..."
-    }
+      return "Coming Soon...";
+    },
+    offsetY() {
+      if (this.focusElement <= 1) return 0;
+      else return 300;
+    },
   },
   watch: {
     focusElement() {
       const item = this.$select(`row${this.focusElement}`);
-      console.log(item)
       if (item && item.$focus) item.$focus();
     },
   },
   hooks: {
     async init() {
-      this.itemId = window.location.href.split("/")[
-        window.location.href.split("/").length - 1
-      ]
+      this.itemId =
+        window.location.href.split("/")[
+          window.location.href.split("/").length - 1
+        ];
       this.similarItems = await fetchSimilarContent(this.itemId);
-      this.itemDetail = await fetchItemDetail(this.itemId)
-      this.imgSrc = getImageUrl(this.itemDetail.backdrop_path, "w1280")
+      this.itemDetail = await fetchItemDetail(this.itemId);
+      this.imgSrc = getImageUrl(this.itemDetail.backdrop_path, "w1280");
     },
     ready() {
       this.$trigger("focusElement");
     },
     focus() {
-      console.log("focus");
       this.scaleBtn = 1.1;
-      this.btnBgColor = "#fff";
+      this.btnBgColor = "#000";
     },
     unfocus() {
       this.scaleBtn = 1;
@@ -71,66 +78,89 @@ export default Blits.Component("ItemDetail", {
   template: `
     <Element w="1920" h="1080" color="#1e293b">
       <Background :bgImg="$imgSrc" alpha="0.5" />
+      <Element
+        w="1920"
+        h="1080"
+        :y.transition="{value: 0-$offsetY, delay: 200, easing: 'cubic-bezier(0.20, 1.00, 0.80, 1.00)'}"
+      >
+        <Text
+          :content="$itemDetail.title"
+          font="raleway"
+          color="#eee"
+          size="80"
+          x="100"
+          y="60"
+          maxwidth="1000"
+          maxlines="1"
+        />
+        <Text :content="$getReleaseDate" size="25" color="#eee" x="100" y="200" />
+        <Element
+          :for="(genre, index) in $itemDetail.genres"
+          :x="($index * 120) + 300"
+          y="200"
+          h="22"
+          color="{left: '#fff', right: '#8a9199'}"
+          maxlines="1"
+          maxwidth="100"
+          :key="$genre.id"
+          :effects="[{type: 'radius', props: {radius: 5}}]"
+        >
+          <Text :content="$genre.name" align="center" y="3" x="8" lineheight="20" size="18" color="#000" />
+        </Element>
+        <Text :content="'Rating: ' + $itemDetail.vote_average.toFixed(1) + '/10'" size="22" color="#eee" x="100" y="240" />
+        <Text
+          :content="$itemDetail.overview"
+          size="32"
+          color="#ced4da"
+          maxwidth="1080"
+          x="100"
+          y="350"
+          lineheight="45"
+          maxlines="4"
+        />
+        <Element
+          y="520"
+          h="80"
+          x="100"
+          :color="{left: $focusElement === 1 ? '#000' : '#7dcaad', right: '#8a9199'}"
+          :scale="{value: $focusElement === 1 ? 1.1 : 1, duration: 200}"
+          maxlines="1"
+          maxwidth="400"
+          :effects="[{type: 'radius', props: {radius: 5}}]"
+          ref="row1"
+        >
+          <Text
+            content="Add to Watchlist"
+            placement="{x: 'center', y: 'middle'}"
+            :color="$focusElement === 1 ? '#fff' : '#000'"
+          />
+        </Element>
+      </Element>
       <Text
-        :content="$itemDetail.title"
+        content="Similar Content for You"
         font="raleway"
         color="#eee"
-        size="80"
+        size="40"
         x="100"
-        y="60"
-        maxwidth="1000"
-        maxlines="1"
+        :y.transition="{value: 700-$offsetY, delay: 200, easing: 'cubic-bezier(0.20, 1.00, 0.80, 1.00)'}"
       />
-      <Text :content="$getReleaseDate" size="25" color="#eee" x="100" y="200" />
-      <Element
-        :for="(genre, index) in $itemDetail.genres"
-        :x="($index * 120) + 300"
-        y="200"
-        h="22"
-        color="{left: '#fff', right: '#8a9199'}"
-        maxlines="1"
-        maxwidth="100"
-        :key="$genre.id"
-        :effects="[{type: 'radius', props: {radius: 5}}]"
-      >
-        <Text :content="$genre.name" align="center" y="3" x="8" lineheight="20" size="18" color="#000" />
-      </Element>
-      <Text :content="'Rating: ' + $itemDetail.vote_average.toFixed(1) + '/10'" size="22" color="#eee" x="100" y="240" />
-      <Text
-        :content="$itemDetail.overview"
-        size="32"
-        color="#ced4da"
-        maxwidth="1080"
-        x="100"
-        y="350"
-        lineheight="45"
-        maxlines="4"
-      />
-      <Element
-        y="600"
-        h="80"
-        x="100"
-        :color="{left: $btnBgColor, right: '#8a9199'}"
-        :scale="{value: $scaleBtn, duration: 200}"
-        maxlines="1"
-        maxwidth="400"
-        :effects="[{type: 'radius', props: {radius: 5}}]"
-        ref="row0"
-      >
-        <Text content="Add to Watchlist" placement="{x: 'center', y: 'middle'}" color="#000" />
-      </Element>
     </Element>
   `,
   input: {
-    down () {
-      if (this.focusElement === 0) {
-        this.focusElement = 1;
+    down() {
+      if (this.focusElement < 2) {
+        this.focusElement++;
       }
     },
-    up () {
-      if (this.focusElement === 1) {
-        this.focusElement = 0;
+    up() {
+      if (this.focusElement > 0) {
+        this.focusElement--;
+      }
+    },
+    enter() {
+      if (this.focusElement === 0) {
+        console.log("add to watchlist");
       }
     }
-  }
+  },
 });
